84aedb4cdf9c97242bbe53efc21711f302f12e42
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-02-15 11:51:20 -0300

    Creating signup page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,42 +6,46 @@ import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+selector: 'page-home',
+templateUrl: 'home.html'
 })
 export class HomePage {
 
-  creds : CredentialsDTO = {
-    username : "",
-    password : ""
-  };
+creds : CredentialsDTO = {
+  username : "",
+  password : ""
+};
 
-  constructor(public navCtrl: NavController, public menu: MenuController, public auth: AuthService) {
+constructor(public navCtrl: NavController, public menu: MenuController, public auth: AuthService) {
 
+}
+
+ionViewWillEnter() {
+  this.menu.swipeEnable(false);
+  }
+  ionViewDidLeave() {
+  this.menu.swipeEnable(true);
   }
 
-  ionViewWillEnter() {
-    this.menu.swipeEnable(false);
-    }
-    ionViewDidLeave() {
-    this.menu.swipeEnable(true);
-    }
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriesPage');
+      },
+      error => {});  
+  }
 
-    ionViewDidEnter() {
-      this.auth.refreshToken()
-        .subscribe(response => {
-          this.auth.successfulLogin(response.headers.get('Authorization'));
-          this.navCtrl.setRoot('CategoriesPage');
-        },
-        error => {});  
-    }
+login(){
+  this.auth.authenticate(this.creds)
+  .subscribe(response => {
+    this.auth.successfulLogin(response.headers.get('Authorization'));
+    this.navCtrl.setRoot('CategoriesPage');
+  },
+  error => {});   
+}
 
-  login(){
-    this.auth.authenticate(this.creds)
-    .subscribe(response => {
-      this.auth.successfulLogin(response.headers.get('Authorization'));
-      this.navCtrl.setRoot('CategoriesPage');
-    },
-    error => {});   
+signup(){
+  this.navCtrl.push("SignupPage");
 }
 }
