b3de188e0ffe8b84c620b9c440604fe2804fc30d
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-02-24 14:32:54 -0300

    Loading product images

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProductDTO } from '../../models/product.dto';
 import { ProductService } from '../../services/domain/product.service';
 
@@ -22,8 +23,20 @@ export class ProductsPage {
     let category_id = this.navParams.get('category_id');
     this.productService.findByCategory(category_id).subscribe(response => {
       this.items = response['content'];
+      this.loadImageUrls();
     }, error => {})
 
   };
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.productService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    }
+  }  
 
 }
--- src/services/domain/product.service.ts
+++ src/services/domain/product.service.ts
@@ -12,4 +12,9 @@ export class ProductService {
   findByCategory(category_id : string) {
     return this.http.get(`${API_CONFIG.baseUrl}/products/?categories=${category_id}`);
   }
+
+  getSmallImageFromBucket(id : string) : Observable<any>{
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+    return this.http.get(url, {responseType : 'blob'});
+  }
 }
